3a117b238315051fd402dc8cb2efcd39b6c51a00
Szász Benjámin István <szasz.benjamin@sulinet.csmszki>
2021-09-30 14:10:33 +0200

    muveletek

---
+++ 20210930/muveletek.py
@@ -0,0 +1,45 @@
+'''
+#kerekítés
+print(round(3.1415,3))
+print(round(3.1415,2))
+#hatványozás
+print(pow(2, 8)) #(alap, kitevő)
+#abszolút érték
+print(abs(-12))
+print(min(12,7,9,45,3))
+print(max(12,7,9,45,3))
+'''
+'''
+#pi
+import math
+a=math.pi
+print(a)
+'''
+'''
+from math import * #a program elején hwlyezzük el 
+a=pi
+print(a)
+#néhyzetgyökvonás =sqrt
+print(sqrt(16))
+#kerekítés fel
+print(ceil(3.56))
+#kerekítés le
+print(floor(3.56))
+'''
+
+#1. A néhyzet területe, kerülete.
+a=float (import("Kérem a négyzet területét!"))
+k=4*2
+t,pow(a,2)
+print("a(z)",a,"egység oldalu négyzet kerülete", k,"területe",t)
+print("A négyzet területe",t)
+print("A négyzet kerölete",k)
+#2. A téglalap területe, kerülete.
+
+
+#3. A kör területe(2*r*pI), kerülete(r^2*pI).
+
+
+#4. A kocka térfogata(V=a^3), felszine(A=6*a^2).
+
+#5. A derékszögű háromszög 2 oldalát és befogóját.
